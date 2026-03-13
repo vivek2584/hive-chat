19c0aab8de3b41f938e4a688ea497b8fdc16c3bd
vivek2584 <vivekroy2584@gmail.com>
2026-03-13 19:56:39 +0000

    fix: manual initial bootstrap

--- node/dht.go
+++ node/dht.go
@@ -2,12 +2,34 @@ package node
 
 import (
 	"context"
+	"sync"
 	"time"
 
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	"github.com/libp2p/go-libp2p/core/peer"
 	"go.uber.org/zap"
 )
 
 func (n *Node) Bootstrap(ctx context.Context) {
+	n.log.Info("connecting to default bootstrap peers...")
+	var wg sync.WaitGroup
+	for _, peerAddr := range dht.DefaultBootstrapPeers {
+		peerinfo, err := peer.AddrInfoFromP2pAddr(peerAddr)
+		if err != nil {
+			continue
+		}
+		wg.Add(1)
+		go func(pi *peer.AddrInfo) {
+			defer wg.Done()
+			if err := n.host.Connect(ctx, *pi); err != nil {
+				n.log.Warn("failed to connect to bootstrap peer", zap.String("peer", pi.ID.String()), zap.Error(err))
+			} else {
+				n.log.Info("connected to bootstrap peer", zap.String("peer", pi.ID.String()))
+			}
+		}(peerinfo)
+	}
+	wg.Wait()
+
 	go func() {
 		err := n.idht.Bootstrap(ctx)
 
@@ -15,8 +37,6 @@ func (n *Node) Bootstrap(ctx context.Context) {
 			n.log.Error("failed to bootstrap DHT", zap.Error(err))
 			return
 		}
-
-		n.log.Info("Bootstrap started")
 	}()
 }
 

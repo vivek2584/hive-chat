8a3688aaa88c1c4d2fb702889e34c67d2a97cbe5
vivek2584 <vivekroy2584@gmail.com>
2026-03-13 19:20:24 +0000

    chore: setup main for node discovery testing

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"context"
 	"github.com/vivek2584/hive-chat/identity"
+	"github.com/vivek2584/hive-chat/node"
 	"go.uber.org/zap"
 )
 
 func main() {
 	logger, _ := zap.NewDevelopment()
 	defer logger.Sync()
-	
+
+	ctx := context.Background()
+
 	i := identity.New(logger)
+
+	n, err := node.New(ctx, logger, i)
+
+	if err != nil{
+		return
+	}
+
+	n.Bootstrap(ctx)
+	n.WaitForBootstrap(ctx)
+
+	err = n.StartLocalDiscovery(ctx)
+
+	if err != nil{
+		return
+	}
+
+	err = n.StartGlobalDiscovery(ctx)
+	if err != nil{
+		return
+	}
 
 }
--- node/dht.go
+++ node/dht.go
@@ -2,6 +2,8 @@ package node
 
 import (
 	"context"
+	"time"
+
 	"go.uber.org/zap"
 )
 
@@ -16,4 +18,24 @@ func (n *Node) Bootstrap(ctx context.Context) {
 
 		n.log.Info("Bootstrap started")
 	}()
+}
+
+func (n *Node) WaitForBootstrap(ctx context.Context) {
+	for {
+		select {
+
+		case <-ctx.Done():
+			return
+
+		default:
+			if n.idht.RoutingTable().Size() > 0 {
+				n.log.Info("DHT bootstrap complete",
+					zap.Int("peers", n.idht.RoutingTable().Size()),
+				)
+				return
+			}
+			n.log.Info("waiting for DHT bootstrap...")
+			time.Sleep(time.Second)
+		}
+	}
 }
